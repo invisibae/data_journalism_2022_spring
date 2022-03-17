db6d7f3aac3274958eaf295135f65b4f045d98f2
Greg Morton <gregorymorton96@gmail.com>
2022-03-16 21:28:57 -0400

    pre_lab_08

---
+++ labs/lab_07/lab_07_journal.R
@@ -0,0 +1,40 @@
+?html_elements
+
+
+?html_text()
+
+
+
+
+states_table <- states_table[[1]]
+
+
+states_table %>%
+  View()
+
+wiki_region %>% colnames()
+
+colnames(wiki_region)[1] <- "court"
+
+colnames(ppp_fraud_courts)[1] <- "court"
+
+ppp_fraud_courts <- ppp_fraud_courts %>%
+  as.data.frame()
+
+
+states_table[[1]]
+
+test
+
+each_row_df$sector_url
+
+xpath_employment_table
+
+url_test
+
+each_row_test$url
+
+
+states_table[[1]]
+
+do.call(rbind, state_info)
