ce0a2e9aa9c475c1fce0ff93a754a30cb5a07331
Greg Morton <gregorymorton96@gmail.com>
2022-05-10 17:51:50 -0400

    state project done

--- major_assignments/comic_books/comic_book_journal.R
+++ major_assignments/comic_books/comic_book_journal.R
@@ -31,15 +31,34 @@ get_jobs <- function(page_link) {
 }
 
 
+get_jobs_marvel <- function(page_link) {
+  hero_page = read_html(url(page_link))
+  hero_jobs = match_page %>%
+    html_nodes(".pi-collapse .pi-border-color:nth-child(4) .pi-font") %>%
+    html_text() 
+  return(hero_jobs_marvel)
+}
+
+
+
+
+
 test_url <- "https://dc.fandom.com/wiki/Batman_(Bruce_Wayne)"
 
 
 dc_url <- "https://dc.fandom.com"
 
+marvel_url <- "https://marvel.fandom.com/"
+
 dc_test <- dc_data %>%
   mutate(slug = gsub("([\\])","", dc_data$urlslug)) %>%
   select(name, slug) %>%
   mutate(full_url = paste0(dc_url, slug))
+
+marvel_test <- marvel_data %>%
+  mutate(slug = gsub("([\\])","", marvel_data$urlslug)) %>%
+  select(name, slug) %>%
+  mutate(full_url = paste0(marvel_url, slug))
 
 
 
@@ -59,6 +78,10 @@ reviews <- character()
 
 page<- (dc_test$full_url[1:200])
 
+page_marvel <- (marvel_test$full_url[55])
+
+page_marvel
+
 
 
 gdxg <- lapply(page, function(i) {
@@ -70,6 +93,18 @@ gdxg <- lapply(page, function(i) {
     html_text(),
   error = function(e){NA}  
  )
+})
+
+
+gdxg_marvel <- lapply(page, function(i) {
+  
+  tryCatch(
+    i %>%
+      read_html %>%
+      html_nodes(".pi-collapse .pi-border-color:nth-child(4) .pi-font") %>% 
+      html_text(),
+    error = function(e){NA}  
+  )
 })
 
 lapply(page, function(i) {
@@ -88,8 +123,7 @@ gdxg
 
 finaldf <- do.call(cbind, gdxg) 
 
-rbind(head(dc_test, 200), test_list) %>%
-  View()
+rbind(head(dc_test, 200), test_list)
 
 test_test <- head(dc_test, 200)
 
@@ -102,8 +136,32 @@ test_list <- lapply(gdxg, function(x) if(identical(x, character(0))) NA_characte
 
 do.call(rbind, test_list)
 
-rbind()
   
+rbind(test_test, test_list) %>% 
+  View()
+
+
+
+
+str_replace_all(jobs_test$job, "·", ",")
+
+jobs_test %>% 
+  filter(!is.na(job)) %>%
+  filter(ALIGN == "Good Characters") %>%
+  group_by(job) %>%
+  count(sort = T)
+
+jobs_test %>% 
+  filter(!is.na(job)) %>%
+  filter(ALIGN == "Bad Characters") %>%
+  group_by(job) %>%
+  count(sort = T)
+
+jobs_test$full_url <- dc_test$full_url
+
+jobs_test %>%
+  View()
+
 
 
 
